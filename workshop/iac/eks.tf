e913b70790e1b9b6a627d1e79ece1821adf6ffa5
naube <bertrand.nau@talan.com>
2023-06-01 14:55:48 +0200

    fix(terraform): dependencies

--- workshop/iac/eks.tf
+++ workshop/iac/eks.tf
@@ -44,10 +44,16 @@ module "eks" {
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [
+    module.eks.cluster_id
+  ]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [
+    module.eks.cluster_id
+  ]
 }
 
 provider "kubernetes" {
